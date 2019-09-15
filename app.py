67031267a541216ad15e45d9969bfe4ad39599e1
Waqar Ali <goraya.waqar@gmail.com>
2019-09-15 23:04:04 +0500

    Create app.py
    initial entry point for python application

---
+++ app.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import re
+import sys
+import os
+import os.path
+import argparse
+import warnings
+
+
+def main(argv=sys.argv[1:]):
+    print('Applcation Started')
+
+
+if __name__ == '__main__':  # pragma: no cover
+    main()
